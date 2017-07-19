b42ac4e0de68237888ebd349e2e59554132b221e
brian-hmn <brian.handmade.network+github@gmail.com>
2017-07-19 15:44:13 -0700

    ctray:  Updated build.bat so that it is buildable within this directory structure.
    I also addressed the issue that you can build it from whichever directory you wish, instead of needing to be within the same directory as build.bat.
    
    settings.ctray will be copied if the source file is newer to the build directory.
    
    Finally, added the /nologo option for both calls to surpress Microsoft's logo when building.
    
    The file still assumes that vcvarsall.bat has been run before.  At the moment I do not want to add a call for there are many different versions of Visual Studio, and I know I probably am not using one that others use.

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,21 @@
 @echo off
-pushd ..\..\build
-rc ..\handmade\timer\ctray.rc
-move ..\handmade\timer\ctray.res .\ctray.res
-cl -FC -Zi ..\handmade\timer\ctray.cpp ctray.res user32.lib gdi32.lib shell32.lib
+setlocal
+
+::  We want the root directory of ctray, which is one directory up.
+::  %~dp0 will get the directory of this file, build.bat
+set CTRAY_ROOT_DIR=%~dp0..
+::  By adding .. to the end of the path, we can now use this to get 
+::  evaluated absolute path.
+for /f "delims=" %%G in ("%CTRAY_ROOT_DIR%") do ( set "CTRAY_ROOT_DIR=%%~fG" )
+
+set CTRAY_BUILD_DIR=%CTRAY_ROOT_DIR%\build
+set CTRAY_CODE_DIR=%CTRAY_ROOT_DIR%\code
+
+if not exist "%CTRAY_BUILD_DIR%" ( mkdir "%CTRAY_BUILD_DIR%" )
+
+pushd "%CTRAY_BUILD_DIR%"
+xcopy /D /Y "%CTRAY_CODE_DIR%\settings.ctray" "%CTRAY_BUILD_DIR%\"
+rc /nologo "%CTRAY_CODE_DIR%\ctray.rc"
+move "%CTRAY_CODE_DIR%\ctray.res" .\ctray.res
+cl /nologo /FC /Zi "%CTRAY_CODE_DIR%\ctray.cpp" ctray.res user32.lib gdi32.lib shell32.lib
 popd
